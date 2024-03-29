b2c00398e26eb031a0016cacfc2892c1dcf2e2b7
Printemilio <102258371+Printemilio@users.noreply.github.com>
2024-03-29 10:10:04 +0100

    Update camlbrick_test.ml

--- camlbrick_test.ml
+++ camlbrick_test.ml
@@ -1,5 +1,5 @@
 #use "CPtest.ml";;
-#use "camlbrick.ml";;
+(*#use "camlbrick.ml";;*)
 
 (*Test Vecteur*)
 
@@ -19,71 +19,96 @@ let test_vec2_mult () : unit =
 ;;
 
 (*Test brick*)
+(*
+   type t_camlbrick ={
+      brick_wall : t_caml_table ; 
+      param : t_camlbrick_param
+      }
+    ;;
+
+*)
+   
+let init_game() : t_camlbrick = 
+  let prm :  t_camlbrick_param = make_camlbrick_param() 
+  in
+  let game_test : t_camlbrick = 
+          {           
+            brick_wall = mat_make(prm.world_width , prm.world_bricks_height, BK_empty ) ; 
+            param = prm
+          } 
+  in
+  (
+    game_test.brick_wall.(1).(1) <- BK_block ;
+    game_test.brick_wall.(0).(1) <- BK_simple ;
+    game_test.brick_wall.(1).(0) <- BK_double ;
+    game_test.brick_wall.(0).(0) <- BK_empty ;
+  );
+  game_test
+;;
+
 (*brick_get*)
 let test_brick_get1 () : unit =
-  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(0,1)", (0,1)) in
+  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(0,1)", (init_game(),0,1)) in
   assert_equals_result_m ("result_brick_get 1", BK_simple, l_res)
 ;;
 
 let test_brick_get2 () : unit =
-  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(0,0)", (0,0)) in
+  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(0,0)", (init_game(),0,0)) in
   assert_equals_result_m ("result_brick_get 2", BK_empty, l_res)
 ;;
 
 let test_brick_get3 () : unit =
-  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(1,1)", (1,1)) in
-  assert_equals_result_m ("result_brick_get 3", BK_double, l_res)
+  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(1,1)", (init_game(),1,1)) in
+  assert_equals_result_m ("result_brick_get 3", BK_block, l_res)
 ;;
 
 let test_brick_get4 () : unit =
-  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(1,0)", (1,0)) in
-  assert_equals_result_m ("result_brick_get 4", BK_bonus, l_res)
+  let l_res : t_brick_kind t_test_result = test_exec (brick_get,"brick_get(1,0)", (init_game(),1,0)) in
+  assert_equals_result_m ("result_brick_get 4", BK_double, l_res)
 ;;
 
-(*brick_hit / à refaire car je comprend pas la signature de la fonction*)
+(*brick_hit*)
 
 let test_brick_hit1 () : unit =
-  let l_res : t_brick_kind t_test_result = test_exec (brick_hit,"brick_hit(1,1)", (1,1)) in
-  assert_equals_result_m ("result_brick_hit1", BK_double, l_res)
+  let l_res : t_brick_kind t_test_result = test_exec (brick_hit,"brick_hit(1,0)", (init_game(),1,0)) in
+  assert_equals_result_m ("result_brick_hit1",  BK_simple, l_res)
+;;
+
+let test_brick_hit2 () : unit =
+  let l_res : t_brick_kind t_test_result = test_exec (brick_hit,"brick_hit(1,1)", (init_game(),1,1)) in
+  assert_equals_result_m ("result_brick_hit2", BK_block, l_res)
+;;
+
+let test_brick_hit3 () : unit =
+  let l_res : t_brick_kind t_test_result = test_exec (brick_hit,"brick_hit(0,1)", (init_game(),0,1)) in
+  assert_equals_result_m ("result_brick_hit3", BK_empty, l_res)
+;;
+
+let test_brick_hit4 () : unit =
+  let l_res : t_brick_kind t_test_result = test_exec (brick_hit,"brick_hit(0,0)", (init_game(),0,0)) in
+  assert_equals_result_m ("result_brick_hit4", BK_empty, l_res)
 ;;
 
 (*brick_color*)
-let init_game() : t_camlbrick = 
-  let game_test : t_camlbrick = {           
-          brick_wall : mat_make(...) ; 
-          param = make_camlbrick_param();
-          } in
-  (
-    brick_wall.(1).(1) <- BK_block ;
-    brick_wall.(0).(1) <- BK_simple ;
-    brick_wall.(1).(0) <- BK_double ;
-    brick_wall.(0).(0) <- Bk_empty ;
-
-  );
-  brick_wall
-;;
-
 
 let test_brick_color1 () : unit =
-  let game : t_camlbrick = int_game() in
-  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(1,1)", (game,1,1)) in
+  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(1,1)", (init_game(),1,1)) in
   assert_equals_result_m ("result_brick_color1", BLACK, l_res)
 ;;
 
 let test_brick_color2 () : unit =
-  (***********************)
-  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(0,1)", (0,1)) in
+  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(0,1)", (init_game(),0,1)) in
   assert_equals_result_m ("result_brick_color2", GREEN, l_res)
 ;;
 
 let test_brick_color3 () : unit =
-  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(1,0)", (1,0)) in
+  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(1,0)", (init_game(),1,0)) in
   assert_equals_result_m ("result_brick_color3", ORANGE, l_res)
 ;;
 
 let test_brick_color4 () : unit =
-  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(0,0)", (0,0)) in
-  assert_equals_result_m ("result_brick_color4", GREY, l_res)
+  let l_res : t_camlbrick_color t_test_result = test_exec (brick_color,"brick_color(0,0)", (init_game(),0,0)) in
+  assert_equals_result_m ("result_brick_color4", GRAY, l_res)
 ;;
 
 (*aux_brick_color*)
@@ -121,6 +146,9 @@ test_brick_get3 ();;
 test_brick_get4 ();;
 
 test_brick_hit1 ();;
+test_brick_hit2 ();;
+test_brick_hit3 ();;
+test_brick_hit4 ();;
 
 test_brick_color1 ();;
 test_brick_color2 ();;
